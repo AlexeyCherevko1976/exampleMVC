33602f161aa633042653862d693bc726e8f533f1
Alexey Cherevko <cherevkoalexey1976@mail.ru>
2021-02-20 17:11:22 +0700

    modern pod db

--- Models/Product.cs
+++ Models/Product.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 namespace exampleMVC.Models
 {
     public class Product
-    {
+    { //
         public int Id { get; set; }
         public string Name { get; set; }
         
--- Startup.cs
+++ Startup.cs
@@ -4,12 +4,14 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MobileStore.Models; // пространство моделей
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace exampleMVC
+namespace MobileStore
 {
     public class Startup
     {
