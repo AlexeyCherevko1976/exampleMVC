542260cbc774e430351b8bd3f0f267446a3cb1c4
Alexey Cherevko <cherevkoalexey1976@mail.ru>
2021-02-20 13:34:33 +0700

    nachal

---
+++ Models/MobileContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace exampleMVC.Models
+{
+    public class MobileContext
+    {
+    }
+}
---
+++ Models/Nomenclature.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace exampleMVC.Models
+{
+    public class Nomenclature
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        Product pr1=new Product {Id=1, Name="pipe"};
+    }
+}
